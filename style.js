96984c21c5a423e313e8a04153b75b742fef3b95
cattmassidy528 <82040819+cattmassidy528@users.noreply.github.com>
2023-01-31 07:33:07 -0500

    Create style.js

---
+++ style.js
@@ -0,0 +1,113 @@
+body {
+    background-color: rgb(33, 37, 41);
+}
+
+.area {
+    background-image: url('./images/steel-blue.png');
+    align-items: center;
+    outline: 4px solid rgb(160, 221, 218);
+    height: 600px;
+    width: 400px;
+    position: relative;   
+}
+
+.ship {
+    position: absolute;
+    background-color: blue;
+    text-align: center;
+    color: rgb(8, 180, 8);
+    font-size: 38px;
+    border-top-left-radius: 110px;
+    border-top-right-radius: 110px;
+    border: 10px solid blue;
+    border-bottom: 0px;
+}
+
+.enemy {
+    position: absolute;
+    background-color: honeydew;
+    outline: 5px solid crimson;
+    text-align: center;
+    color: crimson;
+    border-radius: 50%;
+    font-size: 20px;
+    top: 5px;
+}
+.typicalObject {
+    font-size: 32px;
+}
+.typicalEasy {
+    font-size: 30px;
+}
+.typicalRhetoric {
+    font-size: 17px;
+}
+.typicalJealousy {
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    font-size: 16px;
+}
+.ham {
+    font-size: 18px;
+}
+.easyJs {
+    
+    font-size: 24px;
+}
+.laser {
+    border-top-right-radius: 50px;
+    border-bottom-right-radius: 50px;
+    height: 18px;
+    width: 48px;
+    font-size: 16px;
+    transform: rotate(270deg);
+    position: absolute;
+    color: blue;
+    background-color: greenyellow;
+}
+
+
+.jsScoreTitle {
+    color: blue;
+    opacity: 35%;
+    font-size: 26px;
+    position: absolute;
+    right: 5px;
+    z-index: 100;
+}
+
+.jsScore {
+    color: blue;
+    font-size: 45px;
+    position: absolute;
+    right: 35px;
+    z-index: 100;
+}
+
+.playerScoreTitle {
+    padding-left: 5px;
+    color: blue;
+    opacity: 35%;
+    font-size: 26px;
+    position: absolute;
+    left: 0px;
+    z-index: 100;
+}
+.playerScore {
+    padding-left: 25px;
+    color: blue;
+    font-size: 45px;
+    position: absolute;
+    left: 0px;
+    z-index: 100;
+}
+.explanation-zone {
+    font-size: 20px;
+    color: white;
+    position:absolute;
+    left: 500px; 
+    top: 10px;
+}
+.title {
+    font-size: 40px;
+    margin: auto;
+}
